1ca813929cfb86f385351b88ef5d1067505e30b2
pudy248 <pudy248@DESKTOP-HVCV69A>
2023-10-04 23:31:34 -0700

    commit

--- src/io/keyboard.c
+++ src/io/keyboard.c
@@ -6,9 +6,9 @@
 
 void handle_key() {
     uint8_t key = inb(0x60);
+    if(key == 0xE0) key = inb(0x60);
     uint8_t release = (key & 0x80) >> 7;
     uint8_t code = key & 0x7f;
-
     char ret = 1;
     switch(code) {
         case 0x2a: shift = !release; break;
